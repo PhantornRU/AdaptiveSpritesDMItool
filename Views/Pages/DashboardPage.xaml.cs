70dd9d733883a0e3f7eee3ae452e182014a00b8c
PhantornRU <stalkerlesha2@mail.ru>
2024-08-08 18:25:01 +1000

    DMI State to WPF Visual

--- Views/Pages/DashboardPage.xaml.cs
+++ Views/Pages/DashboardPage.xaml.cs
@@ -1,5 +1,21 @@
 ﻿using AdaptiveSpritesDMItool.ViewModels.Pages;
+using DMISharp;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Drawing.Processing;
+using SixLabors.ImageSharp.Drawing;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Formats.Bmp;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Windows.Media.Imaging;
 using Wpf.Ui.Controls;
+using SixLabors.ImageSharp.Formats;
+using static System.Formats.Asn1.AsnWriter;
+using System.Windows.Media.Media3D;
+using System.Windows.Media;
+using System.Drawing;
 
 namespace AdaptiveSpritesDMItool.Views.Pages
 {
@@ -13,6 +29,156 @@ namespace AdaptiveSpritesDMItool.Views.Pages
             DataContext = this;
 
             InitializeComponent();
+
+            TestFunction();
+        }
+
+
+
+
+
+
+        private void TestFunction()
+        {
+
+            string path = "TestImages";
+            string fullpath = $"{path}/testBodyHuman.dmi";
+            using DMIFile file = new DMIFile(fullpath);
+
+            Debug.WriteLine($"Loaded {file}({file.States.Count}).");
+
+            DMIState stateFirst = file.States.First();
+
+            //stateCanvasLeftPreview.Background = Brushes.Black;
+
+            //ImageBrush brush = new ImageBrush();
+            //brush.ImageSource = new BitmapImage(new Uri(fullpath, UriKind.Relative));
+            //stateCanvasLeftPreview.Background = brush;
+
+            using Image<Rgba32>? imgState = stateFirst.GetFrame(StateDirection.South, 0);
+            if (imgState is Image<Rgba32> valueOfImage)
+                Console.WriteLine($"image is {valueOfImage}");
+            else
+                Console.WriteLine("image does not have a value");
+
+            //imgState.SaveAsPng($"{path}/export/test1.png");
+
+            //var bytes = imgState.Configuration.ImageFormatsManager.GetEncoder(BmpFormat.Instance);
+            //stateCanvasLeftPreview.DraBackground = bytes;
+            //testImg.Source = imgState;
+
+
+            //byte[] bitmap = new byte[imgState.Width * imgState.Height * Unsafe.SizeOf<Rgba32>()];
+            //imgState.CopyPixelDataTo(bitmap);
+
+            int width = imgState.Width;
+            int height = imgState.Height;
+
+            byte[] pixelBytes = new byte[width * height * Unsafe.SizeOf<Rgba32>()];
+            imgState.CopyPixelDataTo(pixelBytes);
+
+            Rgba32[] pixelArray = new Rgba32[imgState.Width * imgState.Height];
+            imgState.CopyPixelDataTo(pixelArray);
+
+            imgTest.Source = GetBMP(imgState);
+
+            //imgTest.Source = ToImage(pixelBytes, width);
+
+
+            //BitmapImage bm = GetConvertedImage(pixelBytes);
+
+
+            //BitmapSource bitmapSource = BitmapSource.Create(1, 1, 32, 32, PixelFormats.Indexed8 BitmapPalettes.Gray256, pixelBytes, 2);
+
+            //imgTest.Source = bitmapSource;
+
+
+            // !!! ТЕСТОВЫЙ ВЫВОД
+            //using (var image = SixLabors.ImageSharp.Image.LoadPixelData(pixelBytes, width, height))
+            //{
+            //    // Work with the image
+            //}
+
+
+
+
+            //imgTest.Source = GetConvertedImage(pixelBytes);
+            //stateCanvasLeftPreview.Background = GetConvertedImage(pixelBytes);
+
+            //File.WriteAllBytes("test.png", bitmap);
+
+
+
+            //using SixLabors.ImageSharp.Image image = SixLabors.ImageSharp.Image.Load(bitmap);
+            //image.Save("output.jpg");
+        }
+
+        private WriteableBitmap GetBMP(Image<Rgba32> _imgState)
+        {
+            var bmp = new WriteableBitmap(_imgState.Width, _imgState.Height, _imgState.Metadata.HorizontalResolution, _imgState.Metadata.VerticalResolution, PixelFormats.Bgra32, null);
+
+            bmp.Lock();
+            try
+            {
+
+                using Image<Rgba32> _image = _imgState;
+                _image.ProcessPixelRows(accessor =>
+                {
+                    var backBuffer = bmp.BackBuffer;
+
+                    for (var y = 0; y < _imgState.Height; y++)
+                    {
+                        Span<Rgba32> pixelRow = accessor.GetRowSpan(y);
+
+                        for (var x = 0; x < _imgState.Width; x++)
+                        {
+                            var backBufferPos = backBuffer + (y * _imgState.Width + x) * 4;
+                            var rgba = pixelRow[x];
+                            var color = rgba.A << 24 | rgba.R << 16 | rgba.G << 8 | rgba.B;
+
+                            System.Runtime.InteropServices.Marshal.WriteInt32(backBufferPos, color);
+                        }
+                    }
+                });
+
+                bmp.AddDirtyRect(new Int32Rect(0, 0, _imgState.Width, _imgState.Height));
+            }
+            finally
+            {
+                bmp.Unlock();
+            }
+            return bmp;
+        }
+
+        public BitmapImage ToImage(byte[] bytes, int width)
+        {
+            MemoryStream stream = new MemoryStream(bytes);
+            stream.Position = 0;
+            BitmapImage bi = new BitmapImage();
+            bi.BeginInit();
+            bi.CacheOption = BitmapCacheOption.OnLoad;
+            bi.DecodePixelWidth = width; // Width of the image
+            bi.StreamSource = stream;
+            bi.EndInit();
+            return bi;
+        }
+
+        private static BitmapImage GetConvertedImage(byte[] imageData)
+        {
+            if (imageData == null || imageData.Length == 0) return null;
+            var image = new BitmapImage();
+            using (var mem = new MemoryStream(imageData))
+            {
+                mem.Position = 0;
+                image.BeginInit();
+                image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.UriSource = null;
+                image.StreamSource = mem;
+                image.EndInit();
+            }
+            image.Freeze();
+            return image;
         }
     }
 }
--- Views/Pages/StatesEditorPage.xaml.cs
+++ Views/Pages/StatesEditorPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using AdaptiveSpritesDMItool.ViewModels.Pages;
+using Microsoft.VisualBasic;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +11,23 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Wpf.Ui.Controls;
+using DMISharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Drawing.Processing;
+using SixLabors.ImageSharp.Drawing;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Formats;
+using static System.Net.Mime.MediaTypeNames;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.Reflection;
+using SixLabors.ImageSharp.Formats.Bmp;
+using System.Runtime.CompilerServices;
+using System.IO;
 
 namespace AdaptiveSpritesDMItool.Views.Pages
 {
@@ -30,6 +44,77 @@ namespace AdaptiveSpritesDMItool.Views.Pages
             DataContext = this;
 
             InitializeComponent();
+
+            TestFunction();
         }
+
+
+
+
+
+        private void TestFunction()
+        {
+
+            string path = "TestImages";
+            string fullpath = $"{path}/testBodyHuman.dmi";
+            using DMIFile file = new DMIFile(fullpath);
+
+            Debug.WriteLine($"Loaded {file}({file.States.Count}).");
+
+            DMIState stateFirst = file.States.First();
+
+            imageLeftPreview.Source = GetBMPFromDMIState(stateFirst, StateDirection.South);
+            imageState1.Source = GetBMPFromDMIState(stateFirst, StateDirection.North);
+            imageState2.Source = GetBMPFromDMIState(stateFirst, StateDirection.East);
+            imageState3.Source = GetBMPFromDMIState(stateFirst, StateDirection.West);
+        }
+
+        private WriteableBitmap GetBMPFromDMIState(DMIState _state, StateDirection _stateDirection)
+        {
+            using Image<Rgba32>? imgState = _state.GetFrame(_stateDirection, 0);
+            if (imgState is Image<Rgba32> valueOfImage)
+                Console.WriteLine($"image state is {valueOfImage}");
+            else
+                Console.WriteLine("image state does not have a value");
+            return GetBMPFromRGBA32(imgState);
+        }
+
+        private WriteableBitmap GetBMPFromRGBA32(Image<Rgba32> _imgState)
+        {
+            var bmp = new WriteableBitmap(_imgState.Width, _imgState.Height, _imgState.Metadata.HorizontalResolution, _imgState.Metadata.VerticalResolution, PixelFormats.Bgra32, null);
+
+            bmp.Lock();
+            try
+            {
+
+                using Image<Rgba32> _image = _imgState;
+                _image.ProcessPixelRows(accessor =>
+                {
+                    var backBuffer = bmp.BackBuffer;
+
+                    for (var y = 0; y < _imgState.Height; y++)
+                    {
+                        Span<Rgba32> pixelRow = accessor.GetRowSpan(y);
+
+                        for (var x = 0; x < _imgState.Width; x++)
+                        {
+                            var backBufferPos = backBuffer + (y * _imgState.Width + x) * 4;
+                            var rgba = pixelRow[x];
+                            var color = rgba.A << 24 | rgba.R << 16 | rgba.G << 8 | rgba.B;
+
+                            System.Runtime.InteropServices.Marshal.WriteInt32(backBufferPos, color);
+                        }
+                    }
+                });
+
+                bmp.AddDirtyRect(new Int32Rect(0, 0, _imgState.Width, _imgState.Height));
+            }
+            finally
+            {
+                bmp.Unlock();
+            }
+            return bmp;
+        }
+
     }
 }
