153963c1bd13062db223b38954a6eb9291c8259c
PhantornRU <stalkerlesha2@mail.ru>
2024-08-10 05:50:42 +1000

    Modes, Controll Availability, Mirror State, Centralize State, Buttons

--- Views/Pages/StatesEditorPage.xaml.cs
+++ Views/Pages/StatesEditorPage.xaml.cs
@@ -43,12 +43,19 @@ namespace AdaptiveSpritesDMItool.Views.Pages
         StateEditMode currentStateEditMode = StateEditMode.Single;
         StateQuantityMode currentStateQuantityMode = StateQuantityMode.Single;
 
+        /// <summary>
+        /// Determines whether the state is centralized - setting the pixel in the middle of the pixel
+        /// </summary>
+        bool isCentralizedState = true;
+        bool isMirroredState = true;
+
         public StatesEditorPage(StatesEditorViewModel viewModel)
         {
             ViewModel = viewModel;
             DataContext = this;
 
             InitializeComponent();
+            ControllButtonsAvailability();
 
 
 
@@ -98,46 +105,69 @@ namespace AdaptiveSpritesDMItool.Views.Pages
 
         #region User Controller
 
-        private void state_MouseDown(object sender, MouseButtonEventArgs e, StateDirection _stateDirection)
+        private void state_MouseDown(MouseButtonEventArgs e, StateDirection _stateDirection)
         {
 
         }
 
-        private void state_MouseMove(object sender, MouseEventArgs e, StateDirection _stateDirection)
+        private void state_MouseMove(MouseEventArgs e, StateDirection _stateDirection)
         {
 
         }
 
-        private void state_MouseUp(object sender, MouseButtonEventArgs e, StateDirection _stateDirection)
+        private void state_MouseUp(MouseButtonEventArgs e, StateDirection _stateDirection)
         {
             SetPixel(e, _stateDirection);
-
-            switch (currentStateQuantityMode)
-            {
-                case StateQuantityMode.Single:
-                    break;
-                case StateQuantityMode.Parallel:
-                    break;
-                case StateQuantityMode.All:
-                    break;
-            }
 
             switch (currentStateEditMode)
             {
                 case StateEditMode.Single:
+                    EditSingleMode(e, _stateDirection);
                     break;
                 case StateEditMode.Fill:
+                    EditFillMode(e, _stateDirection);
                     break;
                 case StateEditMode.Pick:
+                    EditPickMode(e, _stateDirection);
                     break;
                 case StateEditMode.Delete:
+                    EditDeleteMode(e, _stateDirection);
                     break;
                 case StateEditMode.Select:
+                    EditSelectMode(e, _stateDirection);
                     break;
                 case StateEditMode.Move:
+                    EditMoveMode(e, _stateDirection);
                     break;
             }
+        }
 
+        private void EditSingleMode(MouseButtonEventArgs e, StateDirection _stateDirection)
+        {
+            SetPixel(e, _stateDirection);
+        }
+
+        private void EditFillMode(MouseButtonEventArgs e, StateDirection _stateDirection)
+        {
+
+        }
+
+        private void EditPickMode(MouseButtonEventArgs e, StateDirection _stateDirection)
+        {
+
+        }
+
+        private void EditDeleteMode(MouseButtonEventArgs e, StateDirection _stateDirection)
+        {
+
+        }
+
+        private void EditSelectMode(MouseButtonEventArgs e, StateDirection _stateDirection)
+        {
+
+        }
+        private void EditMoveMode(MouseButtonEventArgs e, StateDirection _stateDirection)
+        {
 
         }
 
@@ -146,7 +176,64 @@ namespace AdaptiveSpritesDMItool.Views.Pages
             System.Windows.Media.Color color = System.Windows.Media.Colors.Red;
             System.Drawing.Point mousePos = GetMouseCoordinates(e, stateSourceEditDictionary[_stateDirection]);
             Debug.WriteLine(mousePos);
-            dataImageState.GetBMPstate(_stateDirection, true).SetPixel(mousePos.X, mousePos.Y, color);
+
+
+            //switch (currentStateQuantityMode)
+            //{
+            //    case StateQuantityMode.Single:
+            //        dataImageState.GetBMPstate(_stateDirection, true).SetPixel(mousePos.X, mousePos.Y, color);
+            //        break;
+            //    case StateQuantityMode.Parallel:
+            //        dataImageState.GetBMPstate(_stateDirection, true).SetPixel(mousePos.X, mousePos.Y, color);
+            //        dataImageState.GetBMPstate(_stateDirection+1, true).SetPixel(mousePos.X, mousePos.Y, color);
+            //        break;
+            //    case StateQuantityMode.All:
+            //        foreach (StateDirection tempStateDirection in Enum.GetValues(typeof(StateDirection)))
+            //        {
+            //            if (!stateSourceEditDictionary.Keys.Contains(tempStateDirection))
+            //                continue;
+            //            dataImageState.GetBMPstate(tempStateDirection, true).SetPixel(mousePos.X, mousePos.Y, color);
+            //        }
+            //        break;
+            //}
+
+            int additionValueX = isCentralizedState ? -1 : 0;
+
+            switch (currentStateQuantityMode)
+            {
+                case StateQuantityMode.Single:
+                    dataImageState.GetBMPstate(_stateDirection, true).SetPixel(mousePos.X, mousePos.Y, color);
+                    break;
+
+                case StateQuantityMode.Parallel:
+                    dataImageState.GetBMPstate(_stateDirection, true).SetPixel(mousePos.X, mousePos.Y, color);
+                    StateDirection parallelStateDirection = _stateDirection + 1;
+                    int tempXp = mousePos.X;
+                    if (isMirroredState)
+                    {
+                        //additionValueX = (int)parallelStateDirection <= 1 ? additionValueX : 0; // чтобы было нормальное центрирование на востоке/западе
+                        tempXp = (int)dataImageState.GetBMPstate(parallelStateDirection, true).Width - mousePos.X - 1 + additionValueX;
+                    }
+                    dataImageState.GetBMPstate(parallelStateDirection, true).SetPixel(tempXp, mousePos.Y, color);
+                    break;
+
+                case StateQuantityMode.All:
+                    foreach (StateDirection tempStateDirection in Enum.GetValues(typeof(StateDirection)))
+                    {
+                        if(!stateSourceEditDictionary.Keys.Contains(tempStateDirection))
+                            continue;
+                        int tempX = mousePos.X;
+                        if (isMirroredState && ((int)tempStateDirection % 2 != 0))
+                        {
+                            //additionValueX = (int)tempStateDirection <= 1 ? additionValueX : 0;
+                            tempX = (int)dataImageState.GetBMPstate(tempStateDirection, true).Width - mousePos.X - 1 + additionValueX;
+                        }
+                        dataImageState.GetBMPstate(tempStateDirection, true).SetPixel(tempX, mousePos.Y, color);
+                    }
+                    break;
+            }
+
+
         }
 
         #endregion  User Controller
@@ -166,17 +253,17 @@ namespace AdaptiveSpritesDMItool.Views.Pages
 
         private void imageRightPreviewSouth_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            state_MouseDown(sender, e, StateDirection.South);
+            state_MouseDown(e, StateDirection.South);
         }
 
         private void imageRightPreviewSouth_MouseMove(object sender, MouseEventArgs e)
         {
-            state_MouseMove(sender, e, StateDirection.South);
+            state_MouseMove(e, StateDirection.South);
         }
 
         private void imageRightPreviewSouth_MouseUp(object sender, MouseButtonEventArgs e)
         {
-            state_MouseUp(sender, e, StateDirection.South);
+            state_MouseUp(e, StateDirection.South);
         }
 
         #endregion Mouse Buttons - South Preview
@@ -186,18 +273,18 @@ namespace AdaptiveSpritesDMItool.Views.Pages
 
         private void imageRightPreviewNorth_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            state_MouseDown(sender, e, StateDirection.North);
+            state_MouseDown(e, StateDirection.North);
         }
 
         private void imageRightPreviewNorth_MouseMove(object sender, MouseEventArgs e)
         {
-            state_MouseMove(sender, e, StateDirection.North);
+            state_MouseMove(e, StateDirection.North);
         }
 
         private void imageRightPreviewNorth_MouseUp(object sender, MouseButtonEventArgs e)
         {
 
-            state_MouseUp(sender, e, StateDirection.North);
+            state_MouseUp(e, StateDirection.North);
         }
 
         #endregion Mouse Buttons - North Preview
@@ -207,18 +294,18 @@ namespace AdaptiveSpritesDMItool.Views.Pages
 
         private void imageRightPreviewEast_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            state_MouseDown(sender, e, StateDirection.East);
+            state_MouseDown(e, StateDirection.East);
         }
 
         private void imageRightPreviewEast_MouseMove(object sender, MouseEventArgs e)
         {
-            state_MouseMove(sender, e, StateDirection.East);
+            state_MouseMove(e, StateDirection.East);
         }
 
         private void imageRightPreviewEast_MouseUp(object sender, MouseButtonEventArgs e)
         {
 
-            state_MouseUp(sender, e, StateDirection.East);
+            state_MouseUp(e, StateDirection.East);
         }
 
         #endregion Mouse Buttons - East Preview
@@ -228,17 +315,17 @@ namespace AdaptiveSpritesDMItool.Views.Pages
 
         private void imageRightPreviewWest_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            state_MouseDown(sender, e, StateDirection.West);
+            state_MouseDown(e, StateDirection.West);
         }
 
         private void imageRightPreviewWest_MouseMove(object sender, MouseEventArgs e)
         {
-            state_MouseMove(sender, e, StateDirection.West);
+            state_MouseMove(e, StateDirection.West);
         }
 
         private void imageRightPreviewWest_MouseUp(object sender, MouseButtonEventArgs e)
         {
-            state_MouseUp(sender, e, StateDirection.West);
+            state_MouseUp(e, StateDirection.West);
         }
 
         #endregion Mouse Buttons - West Preview
@@ -251,14 +338,14 @@ namespace AdaptiveSpritesDMItool.Views.Pages
 
         private void SingleButton_Click(object sender, RoutedEventArgs e)
         {
-            ClearEditButtons();
+            ResetEditButtons();
             SingleButton.Appearance = ControlAppearance.Primary;
             currentStateEditMode = StateEditMode.Single;
         }
 
         private void FillButton_Click(object sender, RoutedEventArgs e)
         {
-            ClearEditButtons();
+            ResetEditButtons();
             FillButton.Appearance = ControlAppearance.Primary;
 
             currentStateEditMode = StateEditMode.Fill;
@@ -266,14 +353,14 @@ namespace AdaptiveSpritesDMItool.Views.Pages
 
         private void PickButton_Click(object sender, RoutedEventArgs e)
         {
-            ClearEditButtons();
+            ResetEditButtons();
             PickButton.Appearance = ControlAppearance.Primary;
             currentStateEditMode = StateEditMode.Pick;
         }
 
         private void DeleteButton_Click(object sender, RoutedEventArgs e)
         {
-            ClearEditButtons();
+            ResetEditButtons();
             DeleteButton.Appearance = ControlAppearance.Primary;
             currentStateEditMode = StateEditMode.Delete;
         }
@@ -285,14 +372,14 @@ namespace AdaptiveSpritesDMItool.Views.Pages
 
         private void SelectButton_Click(object sender, RoutedEventArgs e)
         {
-            ClearEditButtons();
+            ResetEditButtons();
             SelectButton.Appearance = ControlAppearance.Primary;
             currentStateEditMode = StateEditMode.Select;
         }
 
         private void MoveButton_Click(object sender, RoutedEventArgs e)
         {
-            ClearEditButtons();
+            ResetEditButtons();
             MoveButton.Appearance = ControlAppearance.Primary;
             currentStateEditMode = StateEditMode.Move;
         }
@@ -304,28 +391,51 @@ namespace AdaptiveSpritesDMItool.Views.Pages
 
         private void ChooseSingleStateButton_Click(object sender, RoutedEventArgs e)
         {
-            ClearStatesButtons();
+            ResetStatesButtons();
             ChooseSingleStateButton.Appearance = ControlAppearance.Primary;
             currentStateQuantityMode = StateQuantityMode.Single;
+            ControllButtonsAvailability();
         }
 
         private void ChooseParallelStatesButton_Click(object sender, RoutedEventArgs e)
         {
-            ClearStatesButtons();
+            ResetStatesButtons();
             ChooseParallelStatesButton.Appearance = ControlAppearance.Primary;
             currentStateQuantityMode = StateQuantityMode.Parallel;
+            ControllButtonsAvailability();
         }
 
         private void ChooseAllStatesButton_Click(object sender, RoutedEventArgs e)
         {
-            ClearStatesButtons();
+            ResetStatesButtons();
             ChooseAllStatesButton.Appearance = ControlAppearance.Primary;
             currentStateQuantityMode = StateQuantityMode.All;
+            ControllButtonsAvailability();
+        }
+
+        private void CentralizeStatesButton_Click(object sender, RoutedEventArgs e)
+        {
+            isCentralizedState = !isCentralizedState;
+            CentralizeStatesButton.Appearance = isCentralizedState ? ControlAppearance.Primary : ControlAppearance.Secondary;
+        }
+
+        private void MirrorStatesButton_Click(object sender, RoutedEventArgs e)
+        {
+            isMirroredState = !isMirroredState;
+            MirrorStatesButton.Appearance = isMirroredState ? ControlAppearance.Primary : ControlAppearance.Secondary;
+            ControllButtonsAvailability();
+        }
+
+        private void ControllButtonsAvailability()
+        {
+            MirrorStatesButton.IsEnabled = currentStateQuantityMode != StateQuantityMode.Single;
+            CentralizeStatesButton.IsEnabled = isMirroredState && (currentStateQuantityMode != StateQuantityMode.Single);
+
         }
 
         #endregion Buttons States Controller
 
-        private void ClearEditButtons()
+        private void ResetEditButtons()
         {
             SingleButton.Appearance = ControlAppearance.Secondary;
             FillButton.Appearance = ControlAppearance.Secondary;
@@ -335,7 +445,7 @@ namespace AdaptiveSpritesDMItool.Views.Pages
             MoveButton.Appearance = ControlAppearance.Secondary;
         }
 
-        private void ClearStatesButtons()
+        private void ResetStatesButtons()
         {
             ChooseSingleStateButton.Appearance = ControlAppearance.Secondary;
             ChooseParallelStatesButton.Appearance = ControlAppearance.Secondary;
@@ -343,6 +453,5 @@ namespace AdaptiveSpritesDMItool.Views.Pages
         }
 
         #endregion Buttons Controller
-
     }
 }
